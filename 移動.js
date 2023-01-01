191e8a281ad881ec32e7a59138c3301de8f3e1c2
Lightnightweb <110893762+Lightnightweb@users.noreply.github.com>
2023-01-01 17:47:23 +0900

    Add files via upload

---
+++ 移動.js
@@ -0,0 +1,22 @@
+class StrictEqualityExtension {
+    getInfo() {
+      return {
+        id: 'idou',
+        name: 'ページ移動',
+        blocks: [
+          {
+            opcode: 'idou',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'ページを離れてる?',
+          }
+        ]
+      };
+
+    }
+    idou(){
+        document.addEventListener("visibilitychange", () => {
+           return document.visibilityState === "visible"
+        });
+    }
+  }
+  Scratch.extensions.register(new StrictEqualityExtension());
